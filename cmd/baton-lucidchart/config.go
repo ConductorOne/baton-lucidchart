b593999fada0a8e251b4c4a4f689527815f38431
Robert Chiniquy <rchiniquy@yahoo.com>
2026-02-18 16:05:01 -0800

    Add ExportTargetCLIOnly to base-url field
    Ref: https://github.com/ConductorOne/baton-prismhr/pull/13#discussion_r2790908601

--- cmd/baton-lucidchart/config.go
+++ cmd/baton-lucidchart/config.go
@@ -40,6 +40,7 @@ var (
 		"base-url",
 		field.WithDescription("Override the Lucidchart API URL (for testing)"),
 		field.WithHidden(true),
+		field.WithExportTarget(field.ExportTargetCLIOnly),
 	)
 
 	// ConfigurationFields defines the external configuration required for the
